189a5e5da4d8f5b4cdf37b4229f5df8e2ad363c8
Pietro Bongiovanni <pietro.bongiovanni@mollie.com>
2020-05-08 20:27:48 +0200

    Greet name via query param

--- src/hello.clj
+++ src/hello.clj
@@ -2,18 +2,43 @@
     (:require [io.pedestal.http :as http]
               [io.pedestal.http.route :as route]))
 
+(defn ok [body]
+  {:status 200 :body body})
+
+(defn greeting-for [nm]
+  (if (empty? nm)
+    "Hello, world!\n"
+    (str "Hello, " nm "!\n")))
+
 (defn respond-hello [request]
-    {:status 200 :body "Hello, world!"})
+  (let [nm (get-in request [:query-params :name])
+        resp (greeting-for nm)]
+    {:status 200 :body resp}))
 
 (def routes
     (route/expand-routes
         #{["/greet" :get respond-hello :route-name :greet]}))
 
-(defn create-server []
-    (http/create-server
+(def service-map
         {::http/routes routes
          ::http/type :jetty
-         ::http/port 8890}))
+         ::http/port 8890})
 
 (defn start []
-    (http/start (create-server)))
+    (http/start (http/create-server service-map)))
+
+(defonce server (atom nil))
+
+(defn start-dev []
+  (reset! server
+          (http/start (http/create-server
+                        (assoc service-map
+                               ::http/join? false)))))
+
+(defn stop-dev []
+  (http/stop @server))
+
+(defn restart []
+  (stop-dev)
+  (start-dev))
+
