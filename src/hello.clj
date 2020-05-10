0d37ae5fec5a36ba45a5b132136f50f58aaf8b06
Pietro Bongiovanni <info@pietrobongiovanni.com>
2020-05-10 22:03:20 +0200

    Added body coercion based on content types

--- src/hello.clj
+++ src/hello.clj
@@ -1,6 +1,8 @@
 (ns hello
-    (:require [io.pedestal.http :as http]
-              [io.pedestal.http.route :as route]))
+    (:require [clojure.data.json :as json]
+              [io.pedestal.http :as http]
+              [io.pedestal.http.route :as route]
+              [io.pedestal.http.content-negotiation :as conneg]))
 
 (defn ok [body]
   {:status 200 :body body})
@@ -10,11 +12,22 @@
 
 (def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
 
+(def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
+
+(def content-neg-intc (conneg/negotiate-content supported-types))
+
 (defn greeting-for [nm]
   (cond
     (unmentionables nm) nil
     (empty? nm) "Hello, world!\n"
     :else (str "Hello, " nm "!\n")))
+
+(def echo
+  {:name ::echo
+   :enter (fn [context]
+           (let [request (:request context)
+                response (ok request)]
+           (assoc context :response response)))})
 
 (defn respond-hello [request]
   (let [nm (get-in request [:query-params :name])
@@ -23,9 +36,27 @@
       (ok resp)
       (not-found))))
 
+(def coerce-body
+  {:name ::coerce-body
+   :leave
+   (fn [context]
+     (let [accepted   (get-in context [:request :accept :field] "text/plain")
+           response   (get context :response)
+           body       (get response :body)
+           coerced-body (case accepted
+                          "text/html" body
+                          "text/plain" body
+                          "applicaiton/edn" (pr-str body)
+                          "application/json" (json/write-str body))
+           updated-response (assoc response
+                                   :headers {"Content-Type" accepted}
+                                   :body coerced-body)]
+       (assoc context :response updated-response)))})
+
 (def routes
     (route/expand-routes
-        #{["/greet" :get respond-hello :route-name :greet]}))
+        #{["/greet" :get [coerce-body content-neg-intc respond-hello] :route-name :greet]
+          ["/echo" :get echo]}))
 
 (def service-map
         {::http/routes routes
